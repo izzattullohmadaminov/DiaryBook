8d9151eaa61642f030e9033735de4dbcfb534432
Izzatbek <izzatbekmadaminov7141@gmail.com>
2024-11-08 11:36:08 +0500

    Foydalanuvchi maʼlumotlarini update qilish

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,3 @@
-const { where } = require("sequelize");
 const db = require("../models/index");
 const User = db.user;
 const Diary = db.diary;
@@ -46,4 +45,62 @@ const getMyProfile = async (req, res) => {
     console.log(err);
   }
 };
-module.exports = { getUserProfile, getMyProfile };
+// Desc    Get update profile page
+// Route   GET /user/profile/update
+// Access  Private
+const updateProfilePage = async (req, res) => {
+  const user = req.session.user;
+  try {
+    res.render("user/update-profile", {
+      title: user.name,
+      user: user,
+      isAuthenticated: req.session.isLogged,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+// Desc    Get update profile page
+// Route   POST /user/profile/update
+// Access  Private
+const updateProfile = async (req, res) => {
+  try {
+    const user = await User.findOne({
+      where: { id: req.session.user.id },
+      raw: true,
+    });
+    if (req.body.email === user.email) {
+      return res.redirect("/user/profile/update");
+    }
+
+    const newDetails = await User.update(
+      {
+        name: req.body.name,
+        email: req.body.email,
+      },
+      {
+        where: {
+          id: req.session.user.id,
+        },
+        raw: true,
+        returning: true,
+        plain: true,
+      }
+    );
+    req.session.user = newDetails[1];
+    req.session.save((err) => {
+      if (err) throw err;
+      return res.redirect("/user/profile/my");
+    });
+    res.redirect("/user/profile/my");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = {
+  getUserProfile,
+  getMyProfile,
+  updateProfilePage,
+  updateProfile,
+};
--- router/user.router.js
+++ router/user.router.js
@@ -3,8 +3,12 @@ const router = Router();
 const {
   getUserProfile,
   getMyProfile,
+  updateProfilePage,
+  updateProfile,
 } = require("../controllers/user.controller");
 const { protected } = require("../middleware/auth");
 router.get("/profile/my", protected, getMyProfile);
+router.get("/profile/update", protected, updateProfilePage);
+router.post("/profile/update", protected, updateProfile);
 router.get("/profile/:id", protected, getUserProfile);
 module.exports = router;
